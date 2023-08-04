d8225528efda727aaa4c54dc96ad40fafdce6b99
ElenaVasilkova <vasilkovaea@gmail.com>
2023-08-04 14:43:21 +0000

    Add commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,23 @@
 import os
 from pathlib import Path
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+  dsn="https://79156667b75b41944532d1453d9e6b4d@o4505646920892416.ingest.sentry.io/4505646925086720",
+  integrations=[DjangoIntegration()],
+
+  traces_sample_rate=1.0,
+  send_default_pii=True
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('TOKEN')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.201.177.33', '127.0.0.1', 'localhost', 'kittygramproject.serveblog.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -90,10 +100,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
